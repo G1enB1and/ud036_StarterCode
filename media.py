3ab84861a1e46461d87804efd7fbfe1676d935ef
Glen Bland <33837382+G1enB1and@users.noreply.github.com>
2017-11-21 17:13:12 -0600

    Fixed width to 80 char max per line.

--- media.py
+++ media.py
@@ -7,16 +7,20 @@ class Movie():
         movie_title: a string representing the movie title.
         movie_storyline: a astring representing the movies storyline.
         poster_image: a string representing a url to the movies poster image.
-        youtube_trailer: a string representing  a url to the movies youtube trailer. 
+        youtube_trailer: a string representing  a url to the movies youtube
+        trailer. 
     """
     
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
-        """Inits Movie class with title, storyline, poster_image_url, and trailer_youtube_url."""
+    def __init__(self, movie_title, movie_storyline, poster_image,
+                 trailer_youtube):
+        """Inits Movie class with title, storyline, poster_image_url, and
+        trailer_youtube_url."""
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
     def show_trailer(self):
-        """Opens a webbrowser to the URL of the youtube movie trailer for the movie that called it."""
+        """Opens a webbrowser to the URL of the youtube movie trailer for the
+        movie that called it."""
         webbrowser.open(self.trailer_youtube_url)
