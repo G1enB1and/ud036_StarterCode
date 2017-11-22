c053ce3a39da62a1c698fecf2f8370cc3ee213a0
Glen Bland <33837382+G1enB1and@users.noreply.github.com>
2017-11-21 23:59:28 -0600

    fixed pep8 white space errors

--- media.py
+++ media.py
@@ -1,16 +1,17 @@
 import webbrowser
 
+
 class Movie():
     """This class provides a way to store movie related information.
-    
+
     Attributes:
         movie_title: a string representing the movie title.
         movie_storyline: a astring representing the movies storyline.
         poster_image: a string representing a url to the movies poster image.
         youtube_trailer: a string representing a url to the movies youtube
-        trailer. 
+        trailer.
     """
-    
+
     def __init__(self, movie_title, movie_storyline, poster_image,
                  trailer_youtube):
         """Inits Movie class with title, storyline, poster_image_url, and
